7fa0be7017db45c8aaacb2bd157a3ba9ddc43e67
Nczinn12 <nczinwork@gmail.com>
2026-04-26 10:25:26 -0300

    next off setup

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,5 +1,11 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+} from "lucide-react"
 import Image from "next/image"
 import {
   Sheet,
@@ -12,6 +18,14 @@ import {
 import { quickSearchOptions } from "../_constants/search"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SideBarButton = () => {
   return (
@@ -26,14 +40,43 @@ const SideBarButton = () => {
 
       <SheetContent
         side="right"
-        className="w-80 overflow-x-auto border border-white/10 bg-black/90 p-6 backdrop-blur-xl"
+        className="border-white/ w-80 overflow-x-auto border bg-black/80 p-6 backdrop-blur-xl"
       >
         <SheetHeader>
           <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
 
-        <div className="flex items-center gap-2 border-b border-solid py-5">
-          <Avatar size="lg">
+        <div className="flex items-center justify-between gap-2 border-b border-solid py-5">
+          <h2 className="text-lg">Olá, Faça seu login!</h2>
+          <Dialog>
+            <DialogTrigger
+              render={
+                <Button variant="secondary" size="icon">
+                  <LogInIcon />
+                </Button>
+              }
+            />
+
+            <DialogContent className="rounded-2xl bg-black/10 text-white shadow-[0_8px_30px_rgba(0,0,0,0.5)] backdrop-blur-lg">
+              <DialogHeader className="text-center">
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta do Google
+                </DialogDescription>
+              </DialogHeader>
+
+              <Button variant="glass" className="flex items-center gap-2">
+                <Image
+                  src="/Google.svg"
+                  width={18}
+                  height={18}
+                  alt="Fazer login com o Google"
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+          {/*<Avatar size="lg">
             <AvatarImage src="/AvatarBaarber.png" alt="User" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
@@ -41,7 +84,7 @@ const SideBarButton = () => {
           <div>
             <p className="font-bold">Pedro Gonçalves</p>
             <p className="text-xs">pedrogoncalves@gmail.com</p>
-          </div>
+          </div>*/}
         </div>
 
         <div className="flex flex-col gap-1 border-b border-solid py-5">
---
+++ prisma/migrations/20260426132428_add_auth_tables/migration.sql
@@ -0,0 +1,132 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageURL` on the `BarberShop` table. All the data in the column will be lost.
+  - You are about to drop the `BarbeShopService` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `booking` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `imageUrl` to the `BarberShop` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "BarbeShopService" DROP CONSTRAINT "BarbeShopService_barberId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "booking" DROP CONSTRAINT "booking_serviceId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "booking" DROP CONSTRAINT "booking_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "BarberShop" DROP COLUMN "imageURL",
+ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "BarbeShopService";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "booking";
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verification_tokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "BarberShopService" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "barberId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BarberShopService_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "serviceId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_tokens_identifier_token_key" ON "verification_tokens"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BarberShopService" ADD CONSTRAINT "BarberShopService_barberId_fkey" FOREIGN KEY ("barberId") REFERENCES "BarberShop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "BarberShopService"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
